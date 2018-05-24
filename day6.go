f4614a40acb171a3c6b1f8f289ad96165abbc890
Hinrichsen, Jochen <Jochen.Hinrichsen@partner.commerzbank.com>
2018-05-24 13:18:38 +0200

    feat: array based impl

--- day6.go
+++ day6.go
@@ -1,3 +1,7 @@
+// As a Go specialty, arrays can be used as map keys (slices can't).
+// Therefore impl #1 uses an array and a length (the sample uses 4 banks, the
+// official input is 16 banks), and impl #2 uses a String represenation.
+
 package adventofcode2017
 
 import (
@@ -11,13 +15,15 @@ func dump6(banks []int) {
 	fmt.Println()
 }
 
+type Banks [16]int
+
 // Day6 returns number of redistributions.
-func Day6(banks []int) int {
+func Day6Impl1(banks Banks, activeBanks int) int {
 	// return index and value of max bank
 	max := func() (int, int) {
 		idx := 0
 		blocks := banks[idx]
-		for i := 1; i < len(banks); i++ {
+		for i := 1; i < activeBanks; i++ {
 			// lower bank wins on tie
 			if blocks < banks[i] {
 				idx = i
@@ -26,19 +32,9 @@ func Day6(banks []int) int {
 		}
 		return idx, blocks
 	}
-	seen := make(map[int]bool)
-	configuration := func() int {
-		// use bank blocks as integer value, i.e. '0 2 7 0' -> 270
-		x := 0
-		exp := 1
-		for i := len(banks) - 1; i >= 0; i-- {
-			x += banks[i] * exp
-			exp *= 10
-		}
-		return x
-	}
+	seen := make(map[Banks]bool)
 	next := func(i int) int {
-		return (i + 1) % len(banks)
+		return (i + 1) % activeBanks
 	}
 	redistributions := 0
 	// dump6(banks)
@@ -55,11 +51,10 @@ func Day6(banks []int) int {
 		}
 		redistributions++
 
-		cfg := configuration()
-		if seen[cfg] {
+		if seen[banks] {
 			break
 		}
-		seen[cfg] = true
+		seen[banks] = true
 		// dump6(banks)
 	}
 	return redistributions
--- day6_test.go
+++ day6_test.go
@@ -3,13 +3,30 @@ package adventofcode2017
 import "testing"
 
 func TestDay6Sample(t *testing.T) {
-	banks := []int{0, 2, 7, 0}
+	banks := Banks{0, 2, 7, 0}
 	want := 5
-	got := Day6(banks)
+	got := Day6Impl1(banks, 4)
 	if want != got {
 		t.Fatalf("want %d but got %d\n", want, got)
 	}
 }
 
-func TestDay6(t *testing.T) {
+func TestArrayAsKeyInMap(t *testing.T) {
+	var m = make(map[[3]int]bool)
+
+	m[[3]int{1, 2, 3}] = true
+	m[[3]int{2, 3, 4}] = true
+	m[[3]int{2, 3, 4}] = true
+
+	want := 2
+	got := len(m)
+	if want != got {
+		t.Fatalf("want %d but got %d\n", want, got)
+	}
+}
+
+func BenchmarkDay6Impl1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Day6Impl1(Banks{0, 2, 7, 0}, 4)
+	}
 }
