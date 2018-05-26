8609b756939e97dad9b0696cc3c9cceb46e96d33
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2018-05-26 09:25:49 +0200

    feat: day 9

---
+++ day9.go
@@ -0,0 +1,38 @@
+package adventofcode2017
+
+func Day9(stream string) int {
+	score := 0
+	level := 0
+	inGarbage := false
+	// when using range, Go does not allow changing loop index
+	// for i := range stream {
+	for i := 0; i < len(stream); i++ {
+		// Prio 0: skip character after !
+		if stream[i] == '!' {
+			i++
+			continue
+		}
+		// Prio 1: skip garbage
+		if stream[i] == '<' {
+			inGarbage = true
+			continue
+		}
+		if stream[i] == '>' {
+			inGarbage = false
+			continue
+		}
+		if inGarbage {
+			continue
+		}
+		if stream[i] == '{' {
+			level++
+			continue
+		}
+		if stream[i] == '}' {
+			score += level
+			level--
+			continue
+		}
+	}
+	return score
+}
