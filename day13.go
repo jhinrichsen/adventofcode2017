42ab4a4a7096290d6566eb94fd70e479fa526556
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2018-10-09 22:08:59 +0200

    feat: successfully solve day 13 part 1

--- day13.go
+++ day13.go
@@ -1,6 +1,85 @@
 package adventofcode2017
 
-// Day13 returns severity of whole trip through layers.
-func Day13(layers []int) int {
-	return 0
+// Day13UsingSparseArray returns severity of whole trip through layers.
+func Day13UsingSparseArray(layers []int) int {
+	// position of security scanner within one layer
+	positions := make([]int, len(layers))
+	// direction of security scanner, 1: down, -1: up
+	directions := make([]int, len(layers))
+	for i := range directions {
+		directions[i] = -1
+	}
+
+	severity := 0
+	for i := range layers {
+		// Tick
+		// can never be caught on sparse layer
+		caught := layers[i] != 0 && positions[i] == 0
+		if caught {
+			depth := i
+			rng := layers[i]
+			severity += depth * rng
+		}
+		// Tack
+		// ignore any layers already passed
+		for j := i + 1; j < len(layers); j++ {
+			sparse := layers[j] == 0
+			if sparse {
+				continue
+			}
+			// turn around if at first or last position
+			first := positions[j] == 0
+			last := positions[j] == layers[j]-1
+			if first || last {
+				directions[j] *= -1
+			}
+			positions[j] += directions[j]
+		}
+	}
+	return severity
+}
+
+type node struct {
+	Next      *node
+	Depth     int
+	Range     int
+	Position  int // Current position of security scanner 0..Range
+	Direction int
+}
+
+// Tack progresses security scanner one step
+func (a *node) Tack() {
+	// lazy init
+	if a.Direction == 0 {
+		a.Direction = -1
+	}
+
+	// Need to turn around?
+	if a.Position == 0 || a.Position == a.Range-1 {
+		a.Direction *= -1
+	}
+	a.Position += a.Direction
+}
+
+func (a *node) Caught() bool {
+	return a.Position == 0
+}
+
+func (a *node) Severity() int {
+	return a.Depth * a.Range
+}
+
+// Day13UsingList returns severity of whole trip through layers.
+func Day13UsingList(l *node) int {
+	severity := 0
+	for ; l != nil; l = l.Next {
+		// progress through all layers
+		for i := 0; i < l.Depth; i++ {
+			l.Tack()
+		}
+		if l.Caught() {
+			severity += l.Depth * l.Range
+		}
+	}
+	return severity
 }
--- day13_test.go
+++ day13_test.go
@@ -2,56 +2,160 @@ package adventofcode2017
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
 	"strings"
 	"testing"
 )
 
-func TestDay13(t *testing.T) {
-	f, err := os.Open("testdata/day13_sample.txt")
+func parse(filename string, newLayer func(int, int)) error {
+	f, err := os.Open(filename)
 	if err != nil {
-		t.Fatal(err)
+		return err
 	}
 	sc := bufio.NewScanner(f)
-	var m = make(map[int]int)
-	var maxDepth int
 	for sc.Scan() {
 		line := sc.Text()
-		parts := strings.Split(line, ":")
+		parts := strings.Split(line, ": ")
 		if len(parts) != 2 {
-			t.Fatalf("want key: value but got %v\n", line)
+			return fmt.Errorf("want key: value but got %v", line)
 		}
-		key := strings.TrimSpace(parts[0])
-		value := strings.TrimSpace(parts[0])
-		k, err := strconv.Atoi(key)
+		k, err := strconv.Atoi(parts[0])
 		if err != nil {
-			t.Fatalf("want numeric key but got %v\n", key)
+			return fmt.Errorf("want numeric key but got %v", parts[0])
 		}
-		v, err := strconv.Atoi(value)
+		v, err := strconv.Atoi(parts[1])
 		if err != nil {
-			t.Fatalf("want numeric key but got %v\n", value)
+			return fmt.Errorf("want numeric key but got %v", parts[1])
 		}
-		m[k] = v
-		if maxDepth < k {
-			maxDepth = k
-		}
+		newLayer(k, v)
 	}
-	if maxDepth != 6 {
-		t.Fatalf("want max depth = 6 but got %d", maxDepth)
-	}
-	// make sure all entries from file are available
-	if len(m) != 4 {
-		t.Fatalf("want len=4 but got %d", len(m))
-	}
+	return nil
+}
 
+// asSparseArray returns sparse layers of sample, where index is depth and
+// value is range
+func asSparseArray(filename string) ([]int, error) {
+	var m = make(map[int]int)
+	var maxDepth int
+	f := func(depth, range_ int) {
+		m[depth] = range_
+		if maxDepth < depth {
+			maxDepth = depth
+		}
+	}
+	if err := parse(filename, f); err != nil {
+		return nil, err
+	}
 	// convert map to array
 	layers := make([]int, maxDepth+1)
 	for k, v := range m {
 		layers[k] = v
 	}
+	return layers, nil
+}
+func TestDay13UsingSparseArray(t *testing.T) {
+	layers, err := asSparseArray("testdata/day13_sample.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(layers) != 7 {
+		t.Fatalf("want max depth = 7 but got %d", len(layers))
+	}
 	want := 24
-	got := Day13(layers)
+	got := Day13UsingSparseArray(layers)
+	if want != got {
+		t.Fatalf("want %v but got %v", want, got)
+	}
+}
+
+func BenchmarkDay13UsingSparseArray(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		layers, err := asSparseArray("testdata/day13_sample.txt")
+		if err != nil {
+			b.Fatal(err)
+		}
+		Day13UsingSparseArray(layers)
+	}
+}
+
+func asList(filename string) (*node, error) {
+	var head, prev *node
+	f := func(depth, range_ int) {
+		l := &node{
+			Depth: depth,
+			Range: range_,
+		}
+		if head == nil {
+			head = l
+		}
+		if prev != nil {
+			prev.Next = l
+		}
+		prev = l
+	}
+	if err := parse(filename, f); err != nil {
+		return nil, err
+	}
+	return head, nil
+}
+
+func TestDay13UsingList(t *testing.T) {
+	l, err := asList("testdata/day13_sample.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := 24
+	got := Day13UsingList(l)
+	if want != got {
+		t.Fatalf("want %v but got %v", want, got)
+	}
+}
+
+func BenchmarkDay13UsingList(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		l, err := asList("testdata/day13_sample.txt")
+		if err != nil {
+			b.Fatal(err)
+		}
+		Day13UsingList(l)
+	}
+}
+
+func TestDay13Sample(t *testing.T) {
+	filename := "testdata/day13_sample.txt"
+	layers, err := asSparseArray(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := Day13UsingSparseArray(layers)
+
+	l, err := asList(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day13UsingList(l)
+
+	if want != got {
+		t.Fatalf("want %v but got %v", want, got)
+	}
+}
+
+func TestDay13(t *testing.T) {
+	filename := "testdata/day13.txt"
+	layers, err := asSparseArray(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := Day13UsingSparseArray(layers)
+
+	l, err := asList(filename)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day13UsingList(l)
+
 	if want != got {
 		t.Fatalf("want %v but got %v", want, got)
 	}
