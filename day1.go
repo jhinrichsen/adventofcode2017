656cd91c4d5467d8af1defc6145dd4dee13b217f
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2021-08-07 18:13:02 +0200

    feat: add day 1 part 2

--- day1.go
+++ day1.go
@@ -1,14 +1,22 @@
 package adventofcode2017
 
-// Day01 finds the sum of all digits that match the next digit
-func Day01(digits []byte) int {
-	sum := 0
+// Day1Part1 finds the sum of all digits that match the next digit.
+func Day1Part1(digits []byte) (sum uint) {
+	return day1(digits, 1)
+}
+
+// Day1Part1 finds the sum of all digits that match the halfway around digit.
+func Day1Part2(digits []byte) (sum uint) {
+	return day1(digits, len(digits)/2)
+}
+
+func day1(digits []byte, n int) (sum uint) {
 	cycle := len(digits)
 	for i, digit := range digits {
-		succ := (i + 1) % cycle
+		succ := (i + n) % cycle
 		if digit == digits[succ] {
-			sum += int(digit)
+			sum += uint(digit)
 		}
 	}
-	return sum
+	return
 }
--- day1_test.go
+++ day1_test.go
@@ -3,25 +3,25 @@ package adventofcode2017
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
+	"os"
 	"testing"
 )
 
-func TestDay01Part1(t *testing.T) {
+func TestDay1Part1(t *testing.T) {
+	const want = 1029
 	in, err := input()
 	if err != nil {
 		t.Fatal(err)
 	}
-	want := 1029
-	got := Day01(in)
+	got := Day1Part1(in)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func TestDay01Sample(t *testing.T) {
+func TestDay1SamplePart1(t *testing.T) {
 	var ts = []struct {
-		want int
+		want uint
 		in   []byte
 	}{
 		{3, []byte{1, 1, 2, 2}},
@@ -30,7 +30,7 @@ func TestDay01Sample(t *testing.T) {
 		{9, []byte{9, 1, 2, 1, 2, 1, 2, 9}},
 	}
 	for _, tt := range ts {
-		got := Day01(tt.in)
+		got := Day1Part1(tt.in)
 		if tt.want != got {
 			t.Fatalf("want %d but got %d\n", tt.want, got)
 		}
@@ -38,7 +38,7 @@ func TestDay01Sample(t *testing.T) {
 }
 
 func input() ([]byte, error) {
-	buf, err := ioutil.ReadFile("testdata/day1.txt")
+	buf, err := os.ReadFile("testdata/day1.txt")
 	if err != nil {
 		return nil, err
 	}
@@ -54,13 +54,45 @@ func input() ([]byte, error) {
 	return buf, nil
 }
 
-func BenchmarkDay01(b *testing.B) {
+func BenchmarkDay1Part1(b *testing.B) {
 	in, err := input()
 	if err != nil {
 		b.Fatal(err)
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Day01(in)
+		Day1Part1(in)
+	}
+}
+
+// TestDay2SamplePart2 tests day 1, part 2.
+func TestDay1SamplePart2(t *testing.T) {
+	var ts = []struct {
+		want uint
+		in   []byte
+	}{
+		{6, []byte{1, 2, 1, 2}},
+		{0, []byte{1, 2, 2, 1}},
+		{4, []byte{1, 2, 3, 4, 2, 5}},
+		{12, []byte{1, 2, 3, 1, 2, 3}},
+		{4, []byte{1, 2, 1, 3, 1, 4, 1, 5}},
+	}
+	for _, tt := range ts {
+		got := Day1Part2(tt.in)
+		if tt.want != got {
+			t.Fatalf("want %d but got %d\n", tt.want, got)
+		}
+	}
+}
+
+func TestDay1Part2(t *testing.T) {
+	const want = 1220
+	in, err := input()
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := Day1Part2(in)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
 	}
 }
