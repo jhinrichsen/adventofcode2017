1cc6a3ca2a26b91b003ab4abe15852e148380189
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2021-08-08 17:26:48 +0200

    feat: add day 6 part 2

--- day6.go
+++ day6.go
@@ -1,14 +1,27 @@
-// As a Go specialty, arrays can be used as map keys (slices can't).
-// Therefore impl #1 uses an array and a length (the sample uses 4 banks, the
-// official input is 16 banks), and impl #2 uses a String represenation.
-
 package adventofcode2017
 
-// Banks domain model
+// Banks domain model.
+// As a Go specialty, arrays can be used as map keys (slices can't).
+// Therefore impl #1 uses an array and a length (the sample uses 4 banks, the
+// official input is 16 banks).
+
 type Banks [16]int
 
-// Day6Part1 returns number of redistributions.
+// Day6Part1 returns number of redistribution cycles.
 func Day6Part1(banks Banks, activeBanks int) int {
+	_, cycles := Day6(banks, activeBanks)
+	return cycles
+}
+
+// Day6Part2 returns number of redistribution cycles for a configuration already
+// seen.
+func Day6Part2(banks Banks, activeBanks int) int {
+	bs, _ := Day6(banks, activeBanks)
+	_, cycles := Day6(bs, activeBanks)
+	return cycles - 1
+}
+
+func Day6(banks Banks, activeBanks int) (Banks, int) {
 	// return index and value of max bank
 	max := func() (int, int) {
 		idx := 0
@@ -27,7 +40,6 @@ func Day6Part1(banks Banks, activeBanks int) int {
 		return (i + 1) % activeBanks
 	}
 	redistributions := 0
-	// dump6(banks)
 	for {
 		i, n := max()
 		banks[i] = 0
@@ -47,5 +59,5 @@ func Day6Part1(banks Banks, activeBanks int) int {
 		seen[banks] = true
 		// dump6(banks)
 	}
-	return redistributions
+	return banks, redistributions
 }
--- day6_test.go
+++ day6_test.go
@@ -58,3 +58,36 @@ func BenchmarkDay6Part1(b *testing.B) {
 		_ = Day6Part1(Banks{0, 2, 7, 0}, 4)
 	}
 }
+
+func TestDay6Part2Example(t *testing.T) {
+	const want = 4
+	banks := Banks{0, 2, 7, 0}
+	got := Day6Part2(banks, 4)
+	if want != got {
+		t.Fatalf("want %d but got %d\n", want, got)
+	}
+}
+
+func TestDay6Part2(t *testing.T) {
+	const want = 2793
+	const nBanks = 16
+	ss, err := linesFromFilename(filename(6))
+	if err != nil {
+		t.Fatal(err)
+	}
+	fs := strings.Fields(ss[0])
+	if len(fs) != nBanks {
+		t.Fatalf("want %d fields but got %d", nBanks, len(fs))
+	}
+	var banks Banks
+	for i := 0; i < nBanks; i++ {
+		banks[i], err = strconv.Atoi(fs[i])
+		if err != nil {
+			t.Fatalf("error converting col %d: %v", i, err)
+		}
+	}
+	got := Day6Part2(banks, nBanks)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
